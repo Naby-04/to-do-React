f513dc337ffb0a22d6f6eb72bf838a0c3b219496
Naby <nabidev.diop@gmail.com>
2025-02-24 20:55:49 +0000

    all done with class component

--- main.js
+++ main.js
@@ -1,180 +1,153 @@
 const { useState, useEffect } = React; // Importer les hooks de React
 
-function App() {
-  const [task, setTask] = useState("");
-  const [taskDelay, setTaskDelay] = useState("");
-  const [taskDescription, setTaskDescription] = useState("");
-  const [tasks, setTasks] = useState([]);
-  const [tasksDelay, setTasksDelay] = useState([]);
-  const [tasksDescription, setTasksDescription] = useState([]);
-  const handleChange = (event) => {
-    setTask(event.target.value); // Mettre à jour le state en temps réel
-  };
-  const handleTaskDelayChange = (event) => {
-    setTaskDelay(event.target.value); // Mettre à jour le state en temps réel
-  };
-  const handleTaskDescriptionChange = (event) => {
-    setTaskDescription(event.target.value); // Mettre à jour le state en temps réel
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault(); // Empêche le rechargement de la page
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    let storedTasks;
+    try {
+      storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    } catch (e) {
+      storedTasks = []; // En cas d'erreur, on met un tableau vide
+    }
+    this.state = {
+      task: "", // pour stocker la valeur de l'input};
+      tasks: storedTasks || [], // pour stocker la liste des tâches dans le local storage
+      // currentEditIndex: null, // Variable globale pour stocker l'index de l'utilisateur en cours de modification
+      icone: "fa-solid fa-plus",
+      background: "btn btn-active",
+      submitType: this.handleSubmit,
+      currentIndex: null,
+    };
+  }
 
-    if (
-      task.trim() !== "" &&
-      taskDelay.trim() !== "" &&
-      taskDescription.trim() !== ""
-    ) {
-      setTasks([...tasks, task]); // Ajouter la tâche à la liste
-      setTasksDelay([...tasksDelay, taskDelay]); // Ajouter la tâche à la liste
-      setTasksDescription([...tasksDescription, taskDescription]); // Ajouter la tâche à la liste
-      setTask(""); // Réinitialiser l'input après ajout
-      setTaskDelay(""); // Réinitialiser l'input après ajout
-      setTaskDescription(""); // Réinitialiser l'input après ajout
+  handleChange = (event) => {
+    this.setState({ task: event.target.value }); // Mettre à jour le state en temps réel
+  };
+  handleSubmit = (event) => {
+    event.preventDefault(); // Empêche le rechargement de la page
+    console.log("tasks avant mise à jour:", this.state.tasks);
+    // console.table(this.tasks);
+
+    if (this.state.task.trim() !== "") {
+      this.setState({
+        tasks: [...this.state.tasks, this.state.task],
+      });
+      localStorage.setItem("tasks", JSON.stringify(this.state.tasks)); // Enregistrer dans le local storage
+      // Ajouter la tâche à la liste
+      this.setState({ task: "" }); // Réinitialiser l'input après ajout
+      this.setState({ icone: "fa-solid fa-plus" });
+      this.setState({ background: "btn btn-active" });
     }
   };
+  handleCallEdit = (index) => {
+    this.setState({ icone: "fa-solid fa-check" });
+    this.setState({ background: "btn btn-desactive" });
+    this.setState({ submitType: this.handleEdit });
+    // passons à l'appel de notre element dans le input
+    const taskelement = this.state.tasks[index];
+    this.setState({ task: taskelement });
+    this.setState({ currentIndex: index });
 
-  // Afficher les tâches mises à jour
-  useEffect(() => {
-    console.table(tasks);
-    console.table(tasksDelay);
-    console.table(tasksDescription);
-  }, [tasks]);
-  return (
-    <div>
-      <div className="form-control w-50 col-12 col-md-6 mx-auto d-flex mt-5">
-        <input
-          type="text"
-          className="form-control rounded"
-          placeholder="Add item"
-          name="Add item"
-          required
-          disabled
-        />
-        {/* Button trigger modal  */}
-        <button
-          type="button"
-          className="btn btn-active"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          <i className="fa-solid fa-plus"></i>
-        </button>
-        {/* Modal */}
-        <div
-          className="modal fade w-100"
-          id="exampleModal"
-          tabIndex="-1"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="false"
-        >
-          <div className="modal-dialog w-100 modal-dialog-centered">
-            <div className="modal-content w-100">
-              <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">
-                  Add your task's information
-                </h1>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="modal-body">
-                <form className="row p-0">
-                  <div className="form-control mb-3 col-12 col-md-6">
-                    <label htmlFor="taskName" className="w-50 mb-2">
-                      Name your task
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control p-3"
-                      id="taskName"
-                      value={task}
-                      onChange={handleChange}
-                      placeholder="Name"
-                      name="prenom"
-                      required
-                    />
-                  </div>
-                  <div className="form-control mb-3 col-12 col-md-6">
-                    <label htmlFor="taskDelay" className="mb-2 w-50">
-                      Task Delay
-                    </label>
-                    <input
-                      type="date"
-                      className="form-control p-3"
-                      id="taskDelay"
-                      value={taskDelay}
-                      onChange={handleTaskDelayChange}
-                      placeholder="Nom"
-                      name="delay"
-                      required
-                    />
-                  </div>
-                  <div className="form-control mb-3 col-12 col-md-6">
-                    <label htmlFor="TaskDescription" className="w-100 mb-2">
-                      Task Description
-                    </label>
-                    <textarea
-                      className="w-100 p-2"
-                      id="TaskDescription"
-                      value={taskDescription}
-                      onChange={handleTaskDescriptionChange}
-                      name="taskDescription"
-                      required
-                    />
-                  </div>
-                  <div className="modal-footer">
-                    <button
-                      id="addbtn"
-                      onClick={handleSubmit}
-                      className="w-100"
-                      type="submit"
-                      data-bs-dismiss="modal"
-                    >
-                      Ajouter
-                    </button>
-                    <button
-                      type="button"
-                      id="edit"
-                      className="w-100"
-                      data-bs-dismiss="modal"
-                    >
-                      Enregistrer
-                    </button>
-                  </div>
-                </form>
-              </div>
+    //-------------
+    console.log(taskelement);
+    console.log(index);
+
+    console.log("Boutton modification cliqué !");
+  };
+  handleEdit = (event) => {
+    event.preventDefault(); // Empêche le rechargement de la page
+    const updatetableau = [...this.state.tasks];
+
+    // Remplacer l'élément à l'index spécifié
+    updatetableau[this.state.currentIndex] = this.state.task;
+
+    this.setState({ tasks: updatetableau, task: "", currentIndex: null });
+    // Mettre à jour l'état avec le nouveau tableau
+    localStorage.setItem("tasks", JSON.stringify(updatetableau));
+    // ----------------
+    this.setState({ submitType: this.handleSubmit });
+    console.table(this.state.tasks);
+  };
+  handleDelete = (index) => {
+    // pour supprimer l'element nous allons creer une nouvelles liste des taches sans y integrer l'element à supprimer avec la methode filtrer
+    const tableauFiltre = this.state.tasks.filter(
+      (task, taskIndex) => taskIndex !== index
+    );
+    // et maintenant nous allos mettre a jour le local storage
+    // rappel : pour mettre a jour un state on utilise setState()
+    this.setState({ tasks: tableauFiltre }, () => {
+      localStorage.setItem("tasks", JSON.stringify(tableauFiltre));
+    });
+    this.setState({ icone: "fa-solid fa-plus" });
+    this.setState({ background: "btn btn-active" });
+    this.setState({ submitType: "{this.handleSubmit}" });
+    console.log("Boutton de suppression cliqué !");
+  };
+  render() {
+    return (
+      <>
+        <div>
+          <form onSubmit={this.state.submitType}>
+            <div className="form-control w-50 col-12 col-md-6 mx-auto d-flex mt-5">
+              <input
+                type="text"
+                className="form-control rounded"
+                placeholder="Add item"
+                name="task name"
+                value={this.state.task}
+                onChange={this.handleChange}
+                required
+              />
+              {/* Button trigger modal  */}
+              <button type="submit" className={this.state.background}>
+                <i className={this.state.icone}></i>
+              </button>
+            </div>
+          </form>
+          {/* affichage de la liste des taches */}
+          <div className="container task-part">
+            <h2 className="text-center mt-5 mx-auto pb-2">List of tasks</h2>
+            <div>
+              <table className="affichage_tableau mt-3">
+                <thead>
+                  <tr>
+                    <th>Tasks</th>
+                  </tr>
+                </thead>
+                <tbody id="table-body">
+                  {/* Les lignes de données vont être insérées ici par JavaScript  */}
+                  {this.state.tasks.map((task, index) => (
+                    <tr key={index}>
+                      <td
+                        className="d-flex justify-content-between align-items-center"
+                        indexdata={index}
+                      >
+                        {task}
+                        <div>
+                          <button
+                            className="btn btn-warning me-2"
+                            onClick={() => this.handleCallEdit(index)}
+                          >
+                            <i className="fa-solid fa-pencil"></i>
+                          </button>
+                          <button
+                            className="btn btn-danger"
+                            onClick={() => this.handleDelete(index)}
+                          >
+                            <i className="fa-solid fa-trash"></i>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      </div>
-      {/* affichage de la liste des taches */}
-      <div className="container user-part">
-        <h2 className="text-center mt-5 mx-auto pb-2">List of tasks</h2>
-        <table className="affichage_tableau mt-3">
-          <thead>
-            <tr>
-              <th>Task</th>
-              <th>Task details</th>
-              <th>Delay</th>
-            </tr>
-          </thead>
-          <tbody id="table-body">
-            {/* Les lignes de données vont être insérées ici par JavaScript  */}
-            {tasks.map((task, index) => (
-              <tr key={index}>
-                <td>{task}</td>
-                <td>{tasksDescription[index]}</td>
-                <td>{tasksDelay[index]}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+      </>
+    );
+  }
 }
 
 // Affichage du composant dans un élément HTML avec l'id "root"
